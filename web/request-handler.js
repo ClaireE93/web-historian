c9b4c4bb74cf182ebb7804d1778d24109ac30e97
Hack Reactor Student <student@hackreactor.com>
2017-09-15 15:41:16 -0700

    Revert "Bad commit"
    This reverts commit 8ded3f9e6dff499197ac88b8334040b34bc78734.

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,50 +5,23 @@ const urlMod = require('url');
 const httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
-
-const staticFileCb = (err, data) => {
-  if (err) {
-    res.statusCode = 404;
-    res.end();
-  } else {
-    res.statusCode = 200;
-    res.end(data.toString());
-  }
-};
-
-const addUrlCb = (err) => {
-  if (err) {
-    res.statusCode = 404;
-    res.end();
-  } else {
-    console.log('SEDING 302');
-    res.statusCode = 302;
-    res.end();
-  }
-};
-
 exports.handleRequest = function (req, res) {
   const { url, method, headers } = req;
   let statusCode;
   const parsedURL = urlMod.parse(url, true);
 
   if (method === 'GET') {
+    const cb = (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end();
+      } else {
+        res.statusCode = 200;
+        res.end(data.toString());
+      }
+    };
 
-    httpHelpers.serveAssets(res, url, staticFileCb);
-
-  } else if (method === 'POST') {
-
-    // const handleUrlInListResponse = (isInList) => {
-    //   console.log('isInList', isInList);
-    //   if (isInList) {
-    //     httpHelpers.serveAssets(res, '/loading.html', staticFileCb);
-    //   } else {
-    //     archive.addUrlToList(url, addUrlCb);
-    //   }
-    // }
-    //
-    // archive.isUrlInList(url, handleUrlInListResponse);
-
+    httpHelpers.serveAssets(res, url, cb);
   } else {
     res.end(archive.paths.list);
   }
