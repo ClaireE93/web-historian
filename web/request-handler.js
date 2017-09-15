f2b9e22c1f4a66a4532a0d7b21d972e833ede1b0
Hack Reactor Student <student@hackreactor.com>
2017-09-15 12:36:29 -0700

    Passing first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
-var path = require('path');
 var archive = require('../helpers/archive-helpers');
+// const fs = require('fs');
+const path = require('path');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  const fs = require('fs');
+  const { url, method, headers } = req;
+  let statusCode;
+
+  if (url === '/' && method === 'GET') {
+    statusCode = 200;
+    const urlPath = path.join(__dirname, '/public/index.html');
+    console.log('path is', urlPath);
+    fs.readFile(urlPath, (err, data) => {
+      if (err) { throw err; }
+      res.end(data.toString());
+    });
+  } else {
+    res.end(archive.paths.list);
+  }
+
+
 };
